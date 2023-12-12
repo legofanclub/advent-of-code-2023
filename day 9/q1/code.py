5cbaa23aa9b3d812c0fb6973e3dd7b9379509f90
Gavin George <67127770+legofanclub@users.noreply.github.com>
2023-12-11 20:54:49 -0800

    day 9 completed

---
+++ day 9/q1/code.py
@@ -0,0 +1,22 @@
+from itertools import pairwise
+
+# simulate
+
+# time complexity: O(n^2)
+# space complexity: O(n^2)
+# n is line length
+
+f = open("input.txt", "r")
+
+def predict(nums):
+    if not all([x == 0 for x in nums]):
+        lower = [y - x for x, y in pairwise(nums)]
+        return nums[-1] + predict(lower)
+    else:
+        return 0
+
+total = 0
+for line in f:
+    total += predict([int(x) for x in line.split()])
+
+print(total)
